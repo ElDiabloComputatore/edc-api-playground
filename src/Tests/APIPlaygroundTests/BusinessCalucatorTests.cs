8e74b113579f3788e522e4b452958564d3ffa900
ElDiabloComputatore <pramuka.tomas@gmail.com>
2021-02-27 22:44:57 +0100

    Added unit tests
    - added all necessary support for unit testing -> installed NUnit, NUnit3TestAdapter and Microsoft.NET.Test.Sdk to run unit tests
    - 2 simple unit tests added

--- src/Tests/APIPlaygroundTests/BusinessCalucatorTests.cs
+++ src/Tests/APIPlaygroundTests/BusinessCalucatorTests.cs
@@ -1,8 +1,45 @@
-﻿using System;
+﻿using NUnit.Framework;
+using NSubstitute;
+using APIPlaygroundBusiness;
 
 namespace APIPlaygroundBusinessTests
 {
+    [TestFixture]
     public class BusinessCalucatorTests
     {
+        private const string Stonks = "Stonks!";
+        private const string NotStonks = "Not stonks...";
+
+        [Test]
+        public void CalculateBusiness_OddNumber_NotStonksReturned()
+        {
+            // arrange
+            var randomizerMock = Substitute.For<IRandomizer>();
+            randomizerMock.Next().Returns(3);
+
+            var businessCalculator = new BusinessCalculator(randomizerMock);
+
+            // act 
+            var result = businessCalculator.CalculateBusiness();
+
+            // assert
+            Assert.AreEqual(NotStonks, result);
+        }
+
+        [Test]
+        public void CalculateBusiness_EvenNumber_StonksReturned()
+        {
+            // arrange
+            var randomizerMock = Substitute.For<IRandomizer>();
+            randomizerMock.Next().Returns(6);
+
+            var businessCalculator = new BusinessCalculator(randomizerMock);
+
+            // act 
+            var result = businessCalculator.CalculateBusiness();
+
+            // assert
+            Assert.AreEqual(Stonks, result);
+        }
     }
 }
